cd448657e5f11d6e63c38f54483909abac840322
Pedro Henrique Penna <pedrohenriquepenna@gmail.com>
2020-10-02 20:06:01 -0300

    [script] Update: Interactive Install Script

--- install.sh
+++ install.sh
@@ -1,3 +1,3 @@
-cp bash/bashrc $HOME/.bashrc
-cp bash/bash_aliases $HOME/.bash_aliases
-cp tmux/tmux.conf $HOME/.tmux.conf
+cp -i bash/bashrc $HOME/.bashrc
+cp -i bash/bash_aliases $HOME/.bash_aliases
+cp -i tmux/tmux.conf $HOME/.tmux.conf
