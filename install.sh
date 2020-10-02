a22b1c2d43c19ba3b7d646cae3295107ebdf4f8e
Pedro Henrique Penna <pedrohenriquepenna@gmail.com>
2020-10-02 20:07:37 -0300

    [script] Update: Install Vim Config

--- install.sh
+++ install.sh
@@ -1,3 +1,8 @@
 cp -i bash/bashrc $HOME/.bashrc
 cp -i bash/bash_aliases $HOME/.bash_aliases
 cp -i tmux/tmux.conf $HOME/.tmux.conf
+
+# Vim
+cp -i vimrc/vimrc $HOME/.vimrc
+git clone https://github.com/VundleVim/Vundle.vim.git $HOME/.vim/bundle/Vundle.vim
+vim +PluginInstall +qall
